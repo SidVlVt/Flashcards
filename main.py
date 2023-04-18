04ccc94547e415ef111e009925f17c1b67d818ed
eni <Sid.Vl.Vt@gmail.com>
2023-04-18 12:59:43 +0100

    Sub-step 3.1 has been implemented Sub-step 3.2 has been implemented

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+import psycopg2
+
+from config import config
 
 
-#  Step 1
+""" --- Step 1 START --- """
+
+
 #  функция запрашивает у пользователя ссылку и проверяет её
 def get_user_link():
     value_stop = 1
@@ -19,7 +24,11 @@ def get_user_link():
     print('Закрытие программы - досвидания!')
 
 
-#  Step 2
+""" --- Step 1 END --- """
+
+""" --- Step 2 START --- """
+
+
 # функция принимает в себя объект bs4 c параграфами и отдает список со всеми словами из этих параграфов
 def get_all_words(object):
     list_all_words = []
@@ -48,12 +57,53 @@ def parsing(link):
     return title, list_all_words
 
 
+""" --- Step 2 END --- """
+
+""" --- Step 3 START --- """
+
+
+#  функция подключения к BD
+def connection_db():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # read connection parameters
+        params = config()
+
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+
+        # create a cursor
+        cur = conn.cursor()
+
+        # execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+
+        # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+
+""" --- Step 3 END --- """
+
+
 #  создаем главную функцию программы
 def main():
     link = get_user_link()
     title, list_all_words = parsing(link)
-    print(title)
-    print(*list_all_words, sep='\n')
+    print(f'Your page title: {title}')
+    connection_db()
 
 
 #  точка входа в программу
